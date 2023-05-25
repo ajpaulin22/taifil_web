b7f09ceaa9576894715c35e4711faa1661575c96
Lenard Robenta <robentalenard@gmail.com>
2023-05-25 17:22:12 +0800

    Commit admin

--- resources/js/Admin/ManagementRegistration.js
+++ resources/js/Admin/ManagementRegistration.js
@@ -2,7 +2,7 @@
 (function(){
     var tblManagementRegistration = "";
     var data = [
-        {IDcheckbox: 1, IDrow: 1, Name: "Lenard", JobCategories: "Cultivate Agriculture", Show: 1, InterviewDate: "01/01/2023", Company: "Seiko", Age: 27, ToAbroad: 0}
+        {IDcheckbox: 1, IDrow: 1, Name: "Lenard", JobCategories: "Cultivate Agriculture", Program: "TITP", Show: 1, InterviewDate: "01/01/2023", Company: "Seiko", Age: 27, ToAbroad: 0}
     ];
 
     $(document).ready(function(){
@@ -10,6 +10,15 @@
         $(".carousel").carousel({
             interval: 2000
         });  
+
+        $("#Code").change(function(){
+            GetJobCategories();
+            $("#Operations").html("");
+        });
+
+        $("#JobCategories").change(function(){
+            GetOperations();
+        })
 
         $(".show").change(function(){
             var x = $(this).attr('id').split('_')[1];
@@ -22,9 +31,185 @@
         });
     })
 
+    function GetJobCategories(){
+        var shtml = "";
+        if ($("#Code").val() == "1"){
+            shtml = "<option value=''></option>" +
+            '<option value="CultivationAgriculture">Cultivation Agriculture</option>' +
+            '<option value="LivestockAgriculture">Livestock Agriculture</option> ';
+        }
+        else if ($("#Code").val() == "2"){
+            shtml = "<option value=''></option>" +
+            '<option value="FishingBoatFisheries">Fishing Boat Fisheries</option> ' +
+            '<option value="Aquaculture">Aquaculture</option> ';
+        }
+        else{
+            shtml = "<option value=''></option>" +
+            '<option value="WellDrilling">Well Drilling</option> ' +
+            '<option value="BuildingSheetMetalWork">Building Sheet Metal Work</option> ' +
+            '<option value="FreezingAndAirConditioning">Freezing And Air Conditioning</option> ' +
+            '<option value="FixtureMaking">Fixture Making</option> ' +
+            '<option value="Carpentry">Carpentry</option> ' +
+            '<option value="FrameWorking">Frame Working</option> ' +
+            '<option value="ReinforcingBarConstruction">Reinforcing Bar Construction</option> ' +
+            '<option value="Scaffolding">Scaffolding</option> ' +
+            '<option value="BuildingStoneConstruction">Building Stone Construction</option> ' +
+            '<option value="Tiling">Tiling</option> ' +
+            '<option value="TileRoofing">Tile Roofing</option> ' +
+            '<option value="Plastering">Plastering</option> ' +
+            '<option value="Plumbing">Plumbing</option> ' +
+            '<option value="HeatInsulation">Heat Insulation</option>' +
+            '<option value="InteriorFinishing">Interior Finishing</option> ' +
+            '<option value="SashSetting">Sash Setting</option> ' +
+            '<option value="Waterproofing">Waterproofing</option> ' +
+            '<option value="ConcretePressureFeeding">Concrete Pressure Feeding</option> ' +
+            '<option value="WellPointConstruction">Well Point Construction</option>' +
+            '<option value="PaperHanging">Paper Hanging</option> ' +
+            '<option value="ApplicationofConstructionEquipment">Application of Construction Equipment</option> ' +
+            '<option value="FurnaceInstallation">Furnace Installation</option> ';
+        }
+        $("#JobCategories").html(shtml);
+    }
+
+    function GetOperations(){
+        var shtml = "";
+        if ($("#JobCategories").val() == "CultivationAgriculture"){
+            shtml = "<option value='0'></option>" +
+            '<option value="1">Facility Horticulture</option>' +
+            '<option value="2">Upland Field Cropping / Vegetable Growing</option>' +
+            '<option value="3">Fruit Growing</option> ';
+        }
+        else if ($("#JobCategories").val() == "LivestockAgriculture"){
+            shtml = "<option value='0'></option>" +
+            '<option value="1">Hog Raising</option>' +
+            '<option value="2">Poultry Farming (Collecting Chicken Eggs)</option>' +
+            '<option value="3">Dairy</option> ';
+        }
+        else if ($("#JobCategories").val() == "FishingBoatFisheries"){
+            shtml = "<option value='0'></option>" +
+            '<option value="1">Skipjack Pole And Line Fishery</option>' +
+            '<option value="2">Long-line Fishery</option>' +
+            '<option value="3">Squid Jigging</option> ' +
+            '<option value="4">Purse Seine Fishery</option>' +
+            '<option value="5">Trawl And Seine Net Fishery</option>' +
+            '<option value="6">Gill Net Fishery</option> ' +
+            '<option value="7">Set Net Fishery</option>' +
+            '<option value="8">Crab And Shrimp Basket Fishery</option>' +
+            '<option value="9">Stick-held-dipnet Fishery</option> ' ;
+        }
+        else if ($("#JobCategories").val() == "Aquaculture"){
+            shtml = "<option value='0'></option>" +
+            '<option value="1">Scallop and Oyster Farming</option>' ;
+        }
+        else if ($("#JobCategories").val() == "WellDrilling"){
+            shtml = "<option value='0'></option>" +
+            '<option value="1">Percussion Type Well Drilling Operation</option>' +
+            '<option value="2">Rotary Type Well Drilling Operation</option>' ;
+        }
+        else if ($("#JobCategories").val() == "BuildingSheetMetalWork"){
+            shtml = "<option value='0'></option>" +
+            '<option value="1">Duct Sheet Metal Operation</option>' +
+            '<option value="2">Interior And Exterior Sheet Metal Operation</option>' ;
+        }
+        else if ($("#JobCategories").val() == "FreezingAndAirConditioning"){
+            shtml = "<option value='0'></option>" +
+            '<option value="1">Freezing And Air Harmonizing Equipment Installation Work</option>' ;
+        }
+        else if ($("#JobCategories").val() == "FixtureMaking"){
+            shtml = "<option value='0'></option>" +
+            '<option value="1">Hand Processing Work Of Wooden Fixture</option>' ;
+        }
+        else if ($("#JobCategories").val() == "Carpentry"){
+            shtml = "<option value='0'></option>" +
+            '<option value="1">Carpentry Contruction Work</option>' ;
+        }
+        else if ($("#JobCategories").val() == "FrameWorking"){
+            shtml = "<option value='0'></option>" +
+            '<option value="1">Framing Contruction Work</option>' ;
+        }
+        else if ($("#JobCategories").val() == "ReinforcingBarConstruction"){
+                    shtml = "<option value='0'></option>" +
+                    '<option value="1">Assembling Reinforced Rod Bar Work</option>' ;
+        }
+        else if ($("#JobCategories").val() == "Scaffolding"){
+                    shtml = "<option value='0'></option>" +
+                    '<option value="1">Scaffolding Building Work</option>' ;
+        }
+        else if ($("#JobCategories").val() == "BuildingStoneConstruction"){
+                    shtml = "<option value='0'></option>" +
+                    '<option value="1">Stone Processing Work</option>' +
+                    '<option value="2">Work Of Putting Out Stones</option>';
+        }
+        else if ($("#JobCategories").val() == "Tiling"){
+                    shtml = "<option value='0'></option>" +
+                    '<option value="1">Tiling Work</option>' ;
+        }
+        else if ($("#JobCategories").val() == "TileRoofing"){
+                    shtml = "<option value='0'></option>" +
+                    '<option value="1">Tile-roofing Work</option>' ;
+        }
+        else if ($("#JobCategories").val() == "Plastering"){
+                    shtml = "<option value='0'></option>" +
+                    '<option value="1">Plasterers Work</option>' ;
+        }
+        else if ($("#JobCategories").val() == "Plumbing"){
+                    shtml = "<option value='0'></option>" +
+                    '<option value="1">Construction Piping Work</option>' +
+                    '<option value="2">Plant Piping Work</option>' ;
+        }
+        else if ($("#JobCategories").val() == "HeatInsulation"){
+                    shtml = "<option value='0'></option>" +
+                    '<option value="1">Heat-retention and Cool-retention Construction Work</option>' ;
+        }
+        else if ($("#JobCategories").val() == "InteriorFinishing"){
+                    shtml = "<option value='0'></option>" +
+                    '<option value="1">Plastic-material Floor Finishing Construction Work</option>' +
+                    '<option value="2">Carpeting Floor Finishing Construction Work</option>' +
+                    '<option value="3">Metal-made foundation Construction Work</option>' +
+                    '<option value="4">Board Finishing Construction Work</option>' +
+                    '<option value="5">Curtain Installation Work</option>' ;                    
+        }
+        else if ($("#JobCategories").val() == "SashSetting"){
+                    shtml = "<option value='0'></option>" +
+                    '<option value="1">Building Sash Installation Work</option>' ;
+        }
+        else if ($("#JobCategories").val() == "Waterproofing"){
+                    shtml = "<option value='0'></option>" +
+                    '<option value="1">Sealing Water-proof Construction Work</option>' ;
+        }
+        else if ($("#JobCategories").val() == "ConcretePressureFeeding"){
+                    shtml = "<option value='0'></option>" +
+                    '<option value="1">Concrete Pressure Transfer Construction Work</option>' ;
+        }
+        else if ($("#JobCategories").val() == "WellPointConstruction"){
+                    shtml = "<option value='0'></option>" +
+                    '<option value="1">Well-point Construction Work</option>' ;
+        }
+        else if ($("#JobCategories").val() == "PaperHanging"){
+                    shtml = "<option value='0'></option>" +
+                    '<option value="1">Painting Work</option>' ;
+        }
+        else if ($("#JobCategories").val() == "ApplicationofConstructionEquipment"){
+                    shtml = "<option value='0'></option>" +
+                    '<option value="1">Dozing Work</option>' +
+                    '<option value="2">Loading Work</option>' +
+                    '<option value="3">Excavating Work</option>' +
+                    '<option value="4">Road Rolling Work</option>' ;
+        }
+        else if ($("#JobCategories").val() == "FurnaceInstallation"){
+                    shtml = "<option value='0'></option>" +
+                    '<option value="1">Furnace Installation Work</option>' ;
+        }
+        else{
+            shtml = "";
+        }
+        $("#Operations").html(shtml);
+    }
+
     function drawDataTable(){
         $("#tblManagementRegistration").DataTable({
             data: data,
+            searching: false,
             columns: [
                 {
                     title: "<input type='checkbox' id='CheckAllitem' />",
@@ -33,9 +218,10 @@
                     },
                     width: "2%", orderable: false
                 },
-                { title: 'ID', data: "IDrow", width: "3%", className: "dt-center"},
+                { title: 'ID', data: "IDrow", width: "4%", className: "dt-center"},
                 { title: 'Name', data: "Name", width: "18%"},
                 { title: 'JobCategories', data: "JobCategories", width: "17%"},
+                { title: 'Program', data: "Program", width: "5%", className: "dt-center"},
                 { 
                     title: 'Show', 
                     render: function (data, row, meta, ){
--- resources/views/pages/ManagementRegistration.blade.php
+++ resources/views/pages/ManagementRegistration.blade.php
@@ -1,6 +1,6 @@
 
 @push('style')
-    <link rel="stylesheet" href="{{asset('css/admin/ManagementRegistration.css')}}">
+    <!-- <link rel="stylesheet" href="{{asset('css/admin/ManagementRegistration.css')}}"> -->
 @endpush
 
 @extends('layout.layout')
@@ -10,25 +10,39 @@
     .form-control{
         display: inline;
     }
-    .header{
+    .HeaderTable{
         text-align:center;
         padding-bottom:25px !important;
-        
     }
 
 </style>
-<div class="panel panel-inverse" id="main-panel" style="margin: auto; margin-top: 10px; width: 95%; font-size: 14px;" >
+<div class="panel panel-inverse" id="main-panel" style="margin: auto; margin-top: 10px; margin-bottom:250px; width: 95%; font-size: 14px;" >
     <div class="panel-heading">
         <h4 class="panel-title"><span class="fa fa-user"></span>Management Registration</h4>
     </div>
     <div class="panel-body">
         <div class="row">
+            <div class="col-sm-1 mb-2">
+                <div class="form-group">
+                    <label>Type:</label>
+                    <select class="form-control" id="Type" style="width: 70%;">
+                        <option value="1">TITP</option>
+                        <option value="2">SSW</option>
+                        <option value="3">Direct</option>
+                    </select>
+                </div>
+            </div>
             <div class="col-sm-3 mb-2">
                 <div class="form-group">
                     <label>Code:</label>
                     <select class="form-control" id="Code" style="width: 70%;">
                         <option value="1">Agriculture</option>
-                        <option value="2">IT</option>
+                        <option value="2">Fishery</option>
+                        <option value="3">Construction</option>
+                        <!-- <option value="4">Food Manufacturing</option>
+                        <option value="5">Textile</option>
+                        <option value="6">Machinery</option>
+                        <option value="7">Others</option> -->
                     </select>
                 </div>
             </div>
@@ -36,8 +50,9 @@
                 <div class="form-group">
                     <label>Job Categories:</label>
                     <select class="form-control" id="JobCategories" style="width: 70%;">
-                        <option value="1">Agriculture</option>
-                        <option value="2">IT</option>
+                        <option value=""></option>
+                        <option value="CultivationAgriculture">Cultivation Agriculture</option>
+                        <option value="LivestockAgriculture">Livestock Agriculture</option>
                     </select>
                 </div>
             </div>
@@ -45,12 +60,10 @@
                 <div class="form-group">
                     <label>Operations:</label>
                     <select class="form-control" id="Operations" style="width: 70%;">
-                        <option value="1">Agriculture</option>
-                        <option value="2">IT</option>
                     </select>
                 </div>
             </div>
-            <div class="col-sm-3 mb-2">
+            <div class="col-sm-2 mb-2">
                 <div class="form-group">
                     <label>Age:</label>
                     <input type="text" class="form-control" style="width: 15%; text-align: center;"> - <input type="text" class="form-control" style="width: 15%; text-align: center;">
@@ -64,27 +77,31 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-sm-1 mb-2">
+            <div class="col-sm-1 mb-2" style='padding-right:0px; margin-right: 5px; width:8%'>
+                <button type="button" id="btnAdd" class="btn btn-sm btn-primary btn-block" style="width: 150px;"><span class="fa fa-plus"></span><span class="btnLabel">Add Applicant</span></button>
+            </div>
+            <div class="col-sm-1 mb-2" style='padding-right:0px; margin-right: 5px; width:5.33333%'>
                 <button type="button" id="btnEdit" class="btn btn-sm btn-primary btn-block" style="width: 100px;"><span class="fa fa-edit"></span><span class="btnLabel">Edit</span></button>
             </div>
-            <div class="col-sm-1 mb-2">
+            <div class="col-sm-1 mb-2" style='padding-right:0px; margin-right: 5px; width:5.33333%'>
                 <button type="button" id="btnDelete" class="btn btn-sm btn-danger btn-block" style="width: 100px;"><span class="fa fa-trash"></span><span class="btnLabel">Delete</span></button>
             </div>
-            <div class="col-sm-3 offset-7 mb-2">
+            <div class="col-sm-3 offset-6 mb-2">
                 <button type="button" id="btnSave" class="btn btn-sm btn-green btn-block" style="width: 200px;"><span class="fa fa-save"></span><span class="btnLabel">Update Applicant Status</span></button>
             </div>
         </div>
-        <div class="table-responsive xs">
-            <table class="table table-striped table-bordered tbl-100p display" style='border: 1px solid grey' data-adjust="-30" id="tblManagementRegistration">
+        <div class="table-responsive xs " style='overflow-x: visible !important'>
+            <table class="table table-striped table-bordered tbl-100p display" style='width: 100%;' data-adjust="-30" id="tblManagementRegistration">
                 <thead style='border: 1px solid black;'>
                     <tr>
-                        <th rowspan="2" class='header' style='padding-bottom:25px;'></th>
-                        <th rowspan="2" class='header' style='margin:auto;text-align:center;'>ID</th>
-                        <th rowspan="2" class='header'>Name</th>
-                        <th rowspan="2" class='header'>Job Categories</th>
+                        <th rowspan="2" class='HeaderTable' style='padding-bottom:25px;'></th>
+                        <th rowspan="2" class='HeaderTable' style='margin:auto;text-align:center;'>ID</th>
+                        <th rowspan="2" class='HeaderTable'>Name</th>
+                        <th rowspan="2" class='HeaderTable'>Job Categories</th>
+                        <th rowspan="2" class='HeaderTable'>Program</th>
                         <th colspan="3" style='text-align: center;'>Interview History</th>
-                        <th rowspan="2" class='header'>Age</th>
-                        <th rowspan="2" class='header'>Sent To Abroad</th>
+                        <th rowspan="2" class='HeaderTable'>Age</th>
+                        <th rowspan="2" class='HeaderTable'>Sent To Abroad</th>
                     </tr>
                     <tr>
                         <th style='text-align:center'>Show/No Show</th>
@@ -98,6 +115,7 @@
                         <td name="ID" style='text-align: center;'></td>
                         <td name="Name"></td>
                         <td name="JobCategories"></td>
+                        <td name="Program"></td>
                         <td name=Show></td>
                         <td name="InterviewDate"></td>
                         <td name="Company"></td>
