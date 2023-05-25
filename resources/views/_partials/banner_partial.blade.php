9f03db4ef5d5ccaeb1a8718f8b5a828fda5a74bd
Alphy Jay Paulin <aj.paulin@seiko-it.com.ph>
2023-05-25 14:30:21 +0800

    gallery

--- resources/views/_partials/banner_partial.blade.php
+++ resources/views/_partials/banner_partial.blade.php
@@ -1,37 +1,43 @@
 
-
+<div class="banner banner--style2 bg_img">
             <div class="row g-0 justify-content-center">
-                <div id="carouselExampleIndicators" class="carousel slide carousel-fade" data-bs-ride="carousel" style="overflow:hidden;height:90vh;">
+                <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
                     <div class="carousel-indicators">
-                      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
                     </div>
                     <div class="carousel-inner">
-                      <div class="carousel-item active" data-bs-interval="3000">
-                        <img src="{{url('/images/carousel/IMAGE 1.jpg')}}" class="d-block w-100 " alt="...">
-                        <div class="carousel-caption d-none d-md-block">
-                            <h2 style="text-shadow: 1px 1px #3a3a3a;font-family: Gil sans MT Condensed">Tai-Fil Manpower Services Corp.</h2>
-                            <p>Join our international family today! Please call us for more info.</p>
+                      <div class="carousel-item active" style="height: 830px" data-bs-interval="3000">
+                        <img src="{{url('/images/Pictures/helping_elder.jpg')}}" class="d-block h-100 w-100" alt="" style="height: 100%;object-fit:cover;filter:brightness(0.5)">
+                        <div class="carousel-caption top-0 mt-5" style="text-shadow: 5px 2px #000000;">
+                            <h1 class="display-1 fw-bolder text-capitalize text-white" >Tai-Fil Manpower Services Corp.</h1>
+                            <p class="fs-5 text-uppercase">Join our international family today! Please call us for more info.</p>
                         </div>
                       </div>
-                      <div class="carousel-item" data-bs-interval="3000">
-                        <img src="{{url('/images/carousel/manpower.jpeg')}}" class="d-block w-100" alt="...">
+                      <div class="carousel-item" style="height: 830px" data-bs-interval="3000">
+                        <img src="{{url('/images/Pictures/mechanical.jpg')}}" class="d-block h-100 w-100" alt="" style="height: 100%;object-fit:cover;filter:brightness(0.5)">
+                        <div class="carousel-caption top-0 mt-5" style="text-shadow: 5px 2px #000000;">
+                            <h1 class="display-1 fw-bolder text-capitalize text-white ">Tai-Fil Manpower Services Corp.</h1>
+                            <p class="fs-5 text-uppercase">Join our international family today! Please call us for more info.</p>
+                        </div>
                       </div>
-                      <div class="carousel-item" data-bs-interval="3000">
-                        <img src="{{url('/images/carousel/JAP.jpg')}}" class="d-block w-100" alt="...">
+                      <div class="carousel-item" style="height: 830px" data-bs-interval="3000">
+                        <img src="{{url('/images/Pictures/painting.jpg')}}" class="d-block h-100 w-100" alt="" style="height: 100%;object-fit:cover;filter:brightness(0.5)">
+                        <div class="carousel-caption top-0 mt-5" style="text-shadow: 5px 2px #000000;">
+                            <h1 class="display-1 fw-bolder text-capitalize text-white">Tai-Fil Manpower Services Corp.</h1>
+                            <p class="fs-5 text-uppercase">Join our international family today! Please call us for more info.</p>
+                        </div>
                       </div>
                     </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
                       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                       <span class="visually-hidden">Previous</span>
                     </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
                       <span class="carousel-control-next-icon" aria-hidden="true"></span>
                       <span class="visually-hidden">Next</span>
                     </button>
-                </div>
-                
-                <div class="col-lg-4 col-12">
-                </div>
+                  </div>
             </div>
+</div>
