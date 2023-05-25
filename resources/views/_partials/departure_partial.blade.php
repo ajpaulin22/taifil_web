1a95a1eb9a9af8f6c99635e277719e7df42d1140
Alphy Jay Paulin <aj.paulin@seiko-it.com.ph>
2023-05-25 15:25:13 +0800

    departure

--- resources/views/_partials/departure_partial.blade.php
+++ resources/views/_partials/departure_partial.blade.php
@@ -1,120 +1,122 @@
 <div class="" >
     <div class="container p-4">
         <div class="section__wrapper wow fadeInUp" data-wow-duration="1.5s">
-            <div class="row g-0 justify-content-center row-cols-lg-2 row-cols-1 shadow-lg rounded" style="background-image: linear-gradient(to right top, #53aa71, #53aa71, #53aa71, #53aa71, #53aa71, #4ea871, #4aa771, #45a571, #3aa171, #2d9d72, #1d9972, #009572);">
-                <div class="col ">
-                    <div class="about__right p-5">
-                        <div class="about__title">
-                            <h3>Depatures <span id="YearDepartureUI">2023</span></h3>
-                        </div>
-                        <form action="#">
-                            <div class="row g-3">
-                                    <div class="row col-md-3 mt-4">
-                                        <label for="dep_jan" style="padding-top: 4px;" class="col-form-label col-md-4">JAN </label>
-                                        <div class="col-md-8">
-                                            <input type="text" class="form-control text-end" name="dep_jan" id="dep_jan" value="10" readonly/>
-                                        </div>
-                                    </div>
-                                    <div class="row col-md-3 mt-4">
-                                        <label for="dep_feb" style="padding-top: 4px;" class="col-form-label col-md-4">FEB </label>
-                                        <div class="col-md-8">
-                                            <input type="text" class="form-control text-end" name="dep_feb" id="dep_feb" value="11" readonly/>
-                                        </div>
-                                    </div>
-                                    <div class="row col-md-3 mt-4">
-                                        <label for="dep_mar" style="padding-top: 4px;" class="col-form-label col-md-4">MAR </label>
-                                        <div class="col-md-8">
-                                            <input type="text" class="form-control text-end" name="dep_mar" id="dep_mar" value="08" readonly/>
-                                        </div>
-                                    </div>
-                                    <div class="row col-md-3 mt-4">
-                                        <label for="dep_apr" style="padding-top: 4px;" class="col-form-label col-md-4">APR </label>
-                                        <div class="col-md-8">
-                                            <input type="text" class="form-control text-end" name="dep_apr" id="dep_apr" value="14" readonly/>
-                                        </div>
-                                    </div>
-                                    <div class="row col-md-3 mt-4">
-                                        <label for="dep_may" style="padding-top: 4px;" class="col-form-label col-md-4">MAY </label>
-                                        <div class="col-md-8">
-                                            <input type="text" class="form-control text-end" name="dep_may" id="dep_may" value="20" readonly/>
-                                        </div>
-                                    </div>
-                                    <div class="row col-md-3 mt-4">
-                                        <label for="dep_jun" style="padding-top: 4px;" class="col-form-label col-md-4">JUN </label>
-                                        <div class="col-md-8">
-                                            <input type="text" class="form-control text-end" name="dep_jun" id="dep_jun" value="16" readonly/>
-                                        </div>
-                                    </div>
-                                    <div class="row col-md-3 mt-4">
-                                        <label for="dep_jul" style="padding-top: 4px;" class="col-form-label col-md-4">JUL </label>
-                                        <div class="col-md-8">
-                                            <input type="text" class="form-control text-end" name="dep_jul" id="dep_jul" value="19" readonly/>
-                                        </div>
-                                    </div>
-                                    <div class="row col-md-3 mt-4">
-                                        <label for="dep_aug" style="padding-top: 4px;" class="col-form-label col-md-4">AUG </label>
-                                        <div class="col-md-8">
-                                            <input type="text" class="form-control text-end" name="dep_aug" id="dep_aug" value="18" readonly/>
-                                        </div>
-                                    </div>
-                                    <div class="row col-md-3 mt-4">
-                                        <label for="dep_sep" style="padding-top: 4px;" class="col-form-label col-md-4">SEP </label>
-                                        <div class="col-md-8">
-                                            <input type="text" class="form-control text-end" name="dep_sep" id="dep_sep" value="14" readonly/>
-                                        </div>
-                                    </div>
-                                    <div class="row col-md-3 mt-4">
-                                        <label for="dep_oct" style="padding-top: 4px;" class="col-form-label col-md-4">OCT </label>
-                                        <div class="col-md-8">
-                                            <input type="text" class="form-control text-end" name="dep_oct" id="dep_oct" value="20" readonly/>
-                                        </div>
-                                    </div>
-                                    <div class="row col-md-3 mt-4">
-                                        <label for="dep_nov" style="padding-top: 4px;" class="col-form-label col-md-4">NOV </label>
-                                        <div class="col-md-8">
-                                            <input type="text" class="form-control text-end" name="dep_nov" id="dep_nov" value="25" readonly/>
-                                        </div>
-                                    </div>
-                                    <div class="row col-md-3 mt-4">
-                                        <label for="dep_dec" style="padding-top: 4px;" class="col-form-label col-md-4">DEC </label>
-                                        <div class="col-md-8">
-                                            <input type="text" class="form-control text-end" name="dep_dec" id="dep_dec" value="12" readonly/>
-                                        </div>
-                                    </div>
-                            </div>
-                        </form>
-                    </div>
+            <div class="justify-content-center row-cols-lg-2 row-cols-1 shadow-lg rounded" style="background-image: linear-gradient(to right top, #53aa71, #53aa71, #53aa71, #53aa71, #53aa71, #4ea871, #4aa771, #45a571, #3aa171, #2d9d72, #1d9972, #009572);">
+                <div class="about__title text-center w-100 p-2 pt-5">
+                    <h3>DEPARTURES <span id="YearDepartureUI">2023</span></h3>
                 </div>
-                <div class="col ">
-                    <div class="about__right p-5">
-                        <form action="#">
-                            <div class="row justify-content-center">
-                                <div class="col-8">
-                                    <div class="about__title ">
-                                        <h3>Total Departures</h3>
-                                    </div>
-                
-                                    <div class="form-group row mb-3 mt-4">
-                                        <label for="" style="padding-top: 4px;" class="col-3">2023</label>
-                                        <div class="col-3">
-                                            <input type="text" class="form-control text-end" value="102" readonly/>
+                <div class="row w-100">
+                    <div class="col ">
+                        <div class="about__right px-5 pb-3">
+                            <form action="#">
+                                <div class="row g-3">
+                                        <div class="row col-md-3 mt-4">
+                                            <label for="dep_jan" style="padding-top: 4px;" class="col-form-label col-md-4">JAN </label>
+                                            <div class="col-md-8">
+                                                <input type="text" class="form-control text-end" name="dep_jan" id="dep_jan" value="10" readonly/>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div class="form-group row mb-3">
-                                        <label for="" style="padding-top: 4px;" class="col-3">2024</label>
-                                        <div class="col-3">
-                                            <input type="text" class="form-control text-end" value="84" readonly/>
+                                        <div class="row col-md-3 mt-4">
+                                            <label for="dep_feb" style="padding-top: 4px;" class="col-form-label col-md-4">FEB </label>
+                                            <div class="col-md-8">
+                                                <input type="text" class="form-control text-end" name="dep_feb" id="dep_feb" value="11" readonly/>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div class="form-group row mb-3">
-                                        <label for="" style="padding-top: 4px;" class="col-3">2025</label>
-                                        <div class="col-3">
-                                            <input type="text" class="form-control text-end" value="0" readonly/>
+                                        <div class="row col-md-3 mt-4">
+                                            <label for="dep_mar" style="padding-top: 4px;" class="col-form-label col-md-4">MAR </label>
+                                            <div class="col-md-8">
+                                                <input type="text" class="form-control text-end" name="dep_mar" id="dep_mar" value="08" readonly/>
+                                            </div>
+                                        </div>
+                                        <div class="row col-md-3 mt-4">
+                                            <label for="dep_apr" style="padding-top: 4px;" class="col-form-label col-md-4">APR </label>
+                                            <div class="col-md-8">
+                                                <input type="text" class="form-control text-end" name="dep_apr" id="dep_apr" value="14" readonly/>
+                                            </div>
+                                        </div>
+                                        <div class="row col-md-3 mt-4">
+                                            <label for="dep_may" style="padding-top: 4px;" class="col-form-label col-md-4">MAY </label>
+                                            <div class="col-md-8">
+                                                <input type="text" class="form-control text-end" name="dep_may" id="dep_may" value="20" readonly/>
+                                            </div>
+                                        </div>
+                                        <div class="row col-md-3 mt-4">
+                                            <label for="dep_jun" style="padding-top: 4px;" class="col-form-label col-md-4">JUN </label>
+                                            <div class="col-md-8">
+                                                <input type="text" class="form-control text-end" name="dep_jun" id="dep_jun" value="16" readonly/>
+                                            </div>
+                                        </div>
+                                        <div class="row col-md-3 mt-4">
+                                            <label for="dep_jul" style="padding-top: 4px;" class="col-form-label col-md-4">JUL </label>
+                                            <div class="col-md-8">
+                                                <input type="text" class="form-control text-end" name="dep_jul" id="dep_jul" value="19" readonly/>
+                                            </div>
+                                        </div>
+                                        <div class="row col-md-3 mt-4">
+                                            <label for="dep_aug" style="padding-top: 4px;" class="col-form-label col-md-4">AUG </label>
+                                            <div class="col-md-8">
+                                                <input type="text" class="form-control text-end" name="dep_aug" id="dep_aug" value="18" readonly/>
+                                            </div>
+                                        </div>
+                                        <div class="row col-md-3 mt-4">
+                                            <label for="dep_sep" style="padding-top: 4px;" class="col-form-label col-md-4">SEP </label>
+                                            <div class="col-md-8">
+                                                <input type="text" class="form-control text-end" name="dep_sep" id="dep_sep" value="14" readonly/>
+                                            </div>
+                                        </div>
+                                        <div class="row col-md-3 mt-4">
+                                            <label for="dep_oct" style="padding-top: 4px;" class="col-form-label col-md-4">OCT </label>
+                                            <div class="col-md-8">
+                                                <input type="text" class="form-control text-end" name="dep_oct" id="dep_oct" value="20" readonly/>
+                                            </div>
+                                        </div>
+                                        <div class="row col-md-3 mt-4">
+                                            <label for="dep_nov" style="padding-top: 4px;" class="col-form-label col-md-4">NOV </label>
+                                            <div class="col-md-8">
+                                                <input type="text" class="form-control text-end" name="dep_nov" id="dep_nov" value="25" readonly/>
+                                            </div>
+                                        </div>
+                                        <div class="row col-md-3 mt-4">
+                                            <label for="dep_dec" style="padding-top: 4px;" class="col-form-label col-md-4">DEC </label>
+                                            <div class="col-md-8">
+                                                <input type="text" class="form-control text-end" name="dep_dec" id="dep_dec" value="12" readonly/>
+                                            </div>
+                                        </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="col ">
+                        <div class="about__right px-5 pb-3">
+                            <form action="#">
+                                <div class="row justify-content-center">
+                                    <div class="col-8">
+                                        <div class="about__title ">
+                                            <h3>Total Departures</h3>
+                                        </div>
+                    
+                                        <div class="form-group row mb-3 mt-4">
+                                            <label for="" style="padding-top: 4px;" class="col-3">2023</label>
+                                            <div class="col-3">
+                                                <input type="text" class="form-control text-end" value="102" readonly/>
+                                            </div>
+                                        </div>
+                                        <div class="form-group row mb-3">
+                                            <label for="" style="padding-top: 4px;" class="col-3">2024</label>
+                                            <div class="col-3">
+                                                <input type="text" class="form-control text-end" value="84" readonly/>
+                                            </div>
+                                        </div>
+                                        <div class="form-group row mb-3">
+                                            <label for="" style="padding-top: 4px;" class="col-3">2025</label>
+                                            <div class="col-3">
+                                                <input type="text" class="form-control text-end" value="0" readonly/>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        </form>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
